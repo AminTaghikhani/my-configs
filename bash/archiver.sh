fe08f58ba74e836e24c18ff5309beb3ca0eff5f7
Amin Taghikhani <amin76taghikhani@gmail.com>
2020-03-11 11:41:07 +0330

    fix server port

--- bash/archiver.sh
+++ bash/archiver.sh
@@ -12,19 +12,20 @@ read -r -p "Archving Changes? [Y/n] " Changes
 
 read -r -p "user: " USER
 read -r -p "server ip: " IP
+read -r -p "server port: " PORT
 
 if [[ $ALL =~ ^[yY][eE][sS]|[yY]$ ]];then
     echo "start archiving"
     tar -czf archive.tar.gz
     echo "start uploading"
-    scp -P 1242 ./archive.tar.gz $USER@$IP:~/
+    scp -P $PORT ./archive.tar.gz $USER@$IP:~/
 fi
 
 if [[ $Changes =~ ^[yY][eE][sS]|[yY]$ ]];then
     echo "start archiving from changes.txt"
     tar -czf archive.changes.tar.gz -T changes.txt
     echo "start uploading from changes.txt"
-    scp -P 1242 ./archive.changes.tar.gz $USER@$IP:~
+    scp -P $PORT ./archive.changes.tar.gz $USER@$IP:~
 fi
 
 
